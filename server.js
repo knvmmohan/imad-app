e195370eb4b88b3f923143ee0bebf732d354027d
knvmmohan <knvmmohan@users.noreply.github.com>
2017-08-19 04:21:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,10 +87,17 @@ function createTemplate (data) {
     return htmlTemplate;
 }
 
-
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+
 
 
 app.get('/:articleName', function(req, res) {
