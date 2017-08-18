c3ab63bb3477960d9e928f7a951c5a6d791e378e
knvmmohan <knvmmohan@users.noreply.github.com>
2017-08-19 03:35:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML= 'This is mohan';
+//Move the image
+var img = document.getElementById('madi');
+img.onclick = function () {
+  img.style.marginLeft = '100px'; 
+};
